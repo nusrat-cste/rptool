c503a71c39b2f9b0dd667c441437a2dcc6440806
nusrat-cste <nusrat.cste@gmail.com>
2017-05-01 02:45:26 +0600

    environment setup 2 for requirements table

---
+++ app/Models/Requirement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Requirement extends Model
+{
+    protected $table = 'requirements';
+    protected $guarded = [];
+}
