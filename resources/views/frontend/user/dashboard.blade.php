e68f442f5daf98bf0528d6c5e5c340485100baf6
Tahsin Abrar <abrar.cste@gmail.com>
2017-05-06 05:01:23 +0600

    showing projects to the user dashboard that are assigned by admin

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -38,50 +38,44 @@
                                 </li><!--media-->
                             </ul><!--media-list-->
 
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Top rated Projects</h4>
-                                </div><!--panel-heading-->
-
-                                <div class="panel-body">
-                                    <ul class="list-group">
-                                        @foreach($topRatedProjects as $ratedProject)
-                                            <li class="list-group-item">
-                                                {{ $ratedProject->project_name or '(Not set)' }} <span class="badge bg-black">5% liked it</span>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-
-                                </div><!--panel-body-->
-                            </div><!--panel-->
-
                         </div><!--col-md-4-->
 
                         <div class="col-md-8 col-md-pull-4">
 
                             <div class="row">
 
+                                @if(count($projects))
                                 @foreach($projects as $project)
-                                <div class="col-md-6">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                            <h4> {{ $project->project_name or '(Not set)' }} </h4>
-                                            <div>
-                                                <a class="btn btn-primary btn-sm pull-right" href="{{ route('frontend.user.dashboard.project.details', $project->id) }}">
-                                                    View details <i class="fa fa-angle-double-right"></i>
-                                                </a>
-                                            </div>
-                                        </div><!--panel-heading-->
+                                    <div class="col-md-6">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                <h4> {{ $project->project_name or '(Not set)' }} </h4>
+                                                <div>
+                                                    <a class="btn btn-primary btn-sm pull-right" href="{{ route('frontend.user.dashboard.project.details', $project->id) }}">
+                                                        View details <i class="fa fa-angle-double-right"></i>
+                                                    </a>
+                                                </div>
+                                            </div><!--panel-heading-->
 
-                                        <div class="panel-body">
-                                            <p>{{ str_limit($project->additional_info, 60) }}</p>
-                                        </div><!--panel-body-->
-                                    </div><!--panel-->
-                                </div><!--col-md-6-->
-                                @endforeach
+                                            <div class="panel-body">
+                                                <p>{{ str_limit($project->additional_info, 60) }}</p>
+                                            </div><!--panel-body-->
+                                        </div><!--panel-->
+                                    </div><!--col-md-6-->
+                                    @endforeach
+                                @else
+                                    <div class="col-md-12">
+                                        <div class="panel panel-default">
+
+                                            <div class="panel-body">
+                                                <p>Sorry, currently there is no project available for you.</p>
+                                            </div><!--panel-body-->
+                                        </div><!--panel-->
+                                    </div><!--col-md-12-->
+                                @endif
 
                                 <div class="pull-right">
-                                    {{ $projects->links() }}
+                                    {{--{{ $projects->links() }}--}}
                                 </div>
                             </div><!--row-->
 
